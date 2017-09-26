6f15177da8bd2918af69ec47dd6cfd86165121c7
Garrett Serack <garretts@microsoft.com>
2017-09-26 13:50:14 -0700

    better logic and resiliency

--- app.ts
+++ app.ts
@@ -1,90 +1,36 @@
 #!/usr/bin/env node
-
-import * as fs from 'fs';
-import {platform, homedir, arch}  from 'os';
-import {join, normalize} from 'path';
 import {staticfs} from './staticfs'
+import { basePath, fileExists, packageJsonPath, installationPath,dotnetPackageName } from './common'
 
 const patchRequire = require("./fs-monkey/index.js").patchRequire;
-/**
- * helper functions
- */
-
-function writePackagePath(packagePath:string, packageFilePath:string)  {
-  if(! fs.statSync(packageFilePath).isFile() ){
-    throw new Error("Installation failed.");  
-  }
-  console.log(`Recording package path for platform-specific runtime: ${packagePath}`);
-  fs.writeFileSync(join(__dirname, "package-path.json"), JSON.stringify(packagePath));
-}
-
-async function install(targetFolder: string, packageName: string,packagePath:string, packageFilePath:string): Promise<void> {
-  const mainPath = `yarn/${require("yarn/package.json").bin.yarn}`;
-  await new Promise(res => require("mkdirp")(targetFolder, () => res()));
-  process.chdir( targetFolder);
-  process.argv = [process.argv[0],mainPath,'add',packageName, "--force"]
-
-  // setup our stuff for when it's done.
-  process.on("exit", ()=> {
-    writePackagePath(packagePath,packageFilePath);
-  });
-
-  // run yarn!
-  require(mainPath);
-}
-
-const fileExists = (path: string | Buffer): Promise<boolean> => new Promise<boolean>((r, j) => fs.stat(path, err => err ? r(false) : r(true)));
-
-function detectArchitecture(): string {
-  switch (platform()) {
-    case 'darwin':
-      return 'osx';
-    case 'win32':
-      switch (arch()) {
-        case 'x64':
-          return 'win';
-        case 'x86':
-          return 'win-x86';
-      }
-      break;
-    case 'linux':
-      return `linux`;
-  }
-  throw new Error(`Unsupported Platform: ${platform()}`);
-}
-
-/**
- * app
- */
-
-const dotnetVersion = "2.0.0";
-const pathOption = process.env.DOTNET_SHARED_HOME || normalize(`${homedir()}/.net/${dotnetVersion}`);
-const archOption = process.env.DOTNET_SHARED_ARCH || detectArchitecture();
-const packageName = `dotnet-sdk-${dotnetVersion}-${archOption}`;
+const force = process.argv.indexOf("--force") !== -1;
 
 async function main() {
-  // load node_modules from our squish'd fs file.
-  patchRequire(await staticfs(`${__dirname}/static.fs`),true);
+  if( force || !fileExists(packageJsonPath)) {
+    // load node_modules from our squish'd fs file.
+    patchRequire(await staticfs(`${__dirname}/static.fs`),true);
 
-  const force = process.argv.indexOf("--force") !== -1;
-  // derive paths
-  const packagePath = join(pathOption, `node_modules/${packageName}/`);
-  const packageFilePath = packagePath + "package.json";
-  try {
-    // force? => remove folder first
-    if (force) {
-      const rimraf = require('rimraf');
-      await new Promise<void>((res, rej) => require(`rimraf`)(packagePath, (err: any) => err ? rej(err) : res()));
+    try {
+      if (force) {
+        // force => remove folder first
+        await new Promise<void>((res, rej) => require(`rimraf`)(installationPath, (err: any) => err ? rej(err) : res()));
+      }
+
+      // yarn setup
+      const yarnMain = `yarn/${require("yarn/package.json").bin.yarn}`;
+      await new Promise(res => require("mkdirp")(basePath, () => res()));
+      process.chdir(basePath);
+      process.argv = [process.argv[0],yarnMain,'add',dotnetPackageName, "--force","--silent"]
+    
+      // setup our stuff for when it's done.
+      process.on("exit", ()=> console.log(`Installed platform-specific runtime: ${installationPath}`));
+    
+      // run yarn!
+      require(yarnMain);
+    } catch (e) {
+      console.error(e);
+      process.exit(1);
     }
-    // install
-    if (!await fileExists(packageFilePath)) {
-      await install(pathOption, packageName,packagePath,packageFilePath);
-    } else {
-      writePackagePath(packagePath,packageFilePath);
-    }
-  } catch (e) {
-    console.error(e);
-    process.exit(1);
   }
 }
 
--- call.ts
+++ call.ts
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
-
-require(require(require("path").join(__dirname, "package-path.json")));
+import { packageJsonPath, installationPath, dotnetPackageName } from './common'
+// don't go for global installed stuff.
+process.env["DOTNET_MULTILEVEL_LOOKUP"] = "0";
+// try local node_modules 
+try { require(dotnetPackageName); } catch { 
+  // run the dotnet package
+  try { require(installationPath);  } catch {
+    // inline install and try again
+    require("child_process").fork(`${__dirname}/app.js`,["--force"]).on("exit",(code:number,signal:string)=> code == 0 ? require(installationPath):console.error("Unable to install/use dotnet framework."));
+  }
+}
